fd5eb27ff637967189e16e591998d1405e974493
jakub <gaboryjakub@gmail.com>
2018-02-01 22:43:31 +0100

    4th day tasks

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,11 @@
-//Twój kod
+const http = require('http');
+const PORT = 3000;
+
+const server = http.createServer((req, res) => {
+    console.log('Someone is trying to connect with this server');
+    res.end('Hello World from back-end :)');
+});
+
+server.listen(PORT, () => {
+   console.log(`Server running on port ${PORT}`);
+});
